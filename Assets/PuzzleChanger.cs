022f2442eafce9be089ec14c8139ad50acd37624
Gauraang Goel <ggauraang@gmail.com>
2024-12-13 18:27:56 +0530

    Script Modified

---
+++ Assets/PuzzleChanger.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PuzzleChanger : MonoBehaviour
+{
+    public List<GameObject> planetPuzzles;
+    private int currentPuzzleIndex = 0;
+    void Start()
+    {
+        for (int i = 1; i < planetPuzzles.Count; i++)
+        {
+            planetPuzzles[i].SetActive(false);
+        }
+        planetPuzzles[currentPuzzleIndex].SetActive(true);
+    }
+    public void OnNextButtonClicked()
+    {
+        SwitchToNextPuzzle();
+    }
+    private void SwitchToNextPuzzle()
+    {
+        planetPuzzles[currentPuzzleIndex].SetActive(false);
+        currentPuzzleIndex++;
+        planetPuzzles[currentPuzzleIndex].SetActive(true);
+    }
+}
